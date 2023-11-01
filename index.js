18f015dabd6d6228b3b34df64ef7d48000f8f97a
Ka rusnanto <148330242+kaRusnanto@users.noreply.github.com>
2023-11-01 21:35:38 +0700

    Add files via upload

---
+++ index.js
@@ -0,0 +1,30 @@
+class heroes {
+    constructor(){
+        this.nama = ""
+        this.elemen = ""
+        this.kotak = ""
+        this.segitiga = ""
+        this.bulat = ""
+        this.silang = ""
+    }
+
+    menyerang(){
+        console.log(("Ayamnya Terbang"))
+    }
+
+    ulti(serang){
+        console.log("Ayamnya Makan" + makanan)
+    }
+}
+
+Rembo = new Ayam()
+let nama = ""
+let elemen = ""
+let kotak = ""
+let segitiga = ""
+let bulat = ""
+let silang = ""
+
+let makan = Rembo.makan("Bakso Miftah") 
+
+console.log(makan);
